4004edc03d3333840beb1e1fae405b39f27437b3
tyler <tylerscott.home@gmail.com>
2022-10-01 09:54:06 -0400

    Base Logic Done

--- Graph.java
+++ Graph.java
@@ -62,13 +62,82 @@ public class Graph {
         }
       }
     }
-    System.out.println("\n");
-    System.out.println("Array of nodes adjacent to node " + s.name);
-    System.out.println(rtnArray);
+    //System.out.print("DFS("+ s.name + ")" + "\n");
     return rtnArray;
   }
 
   public void findConnectedComponents() {
 
+    ArrayList<Node> initialNode;
+    ArrayList<Node> connectedToS;
+    ArrayList<Node> connectedToV;
+    ArrayList<Node> tempArray = new ArrayList<>();
+
+    // Loop through all Nodes starting at arbitrary Node S
+    for (Node s: this.nodes) {
+
+      // Call DFS on current Node S
+      initialNode = DFS(s);
+      tempArray.add(s); // Add the starting node to array
+      System.out.print("DFS(" + s.name + ")\n");
+
+      // Loop through all available Nodes V, check whether there is an ede
+      // connecting to Node s
+      for (Node v: this.nodes) {
+
+        // If there is an edge from V to S, add to the temp array of connections
+        if (initialNode.contains(v)) {
+
+          // Call DFS on all the Nodes V that have a path to S
+          connectedToS = DFS(v);
+          //System.out.println("V connections to Node S = " + s.name + ": " + connectedToS);
+
+          for (int i = 0; i < connectedToS.size(); i++) {
+
+            // Call DFS one more time to check the array produced by the edges
+            // connecting to Node V
+            if (!tempArray.contains(connectedToS)) {
+              tempArray.add(connectedToS.get(i));
+              connectedToV = DFS(connectedToS.get(i));
+
+              // Loop one last time to check values that are connected to V
+              // TODO: I need to further check these new values, so the nested for loops are probably not the best approach
+              for (int j = 0; j < connectedToV.size(); j++) {
+                tempArray.add(connectedToV.get(j));
+                //System.out.println("Connected to  " + connectedToV.get(j) + ": " + connectedToV);
+
+              }
+            }
+          }
+        }
+      }
+      //System.out.println("Array of All Connections: " + tempArray);
+
+      // Take all connected nodes values from temp array, remove duplicates and sort
+      ArrayList<Node> rtnArray = new ArrayList<>();
+      for (Node n: tempArray) {
+        if (!rtnArray.contains(n)) {
+          rtnArray.add(n);
+        }
+      }
+
+      // Sort the final ArrayList
+      for(int i = 0; i < rtnArray.size(); i++){
+        for(int j = i; j < rtnArray.size(); j++){
+          if(rtnArray.get(i).name > rtnArray.get(j).name){
+            Node tempNode = rtnArray.get(i);
+            rtnArray.set(i, rtnArray.get(j));
+            rtnArray.set(j, tempNode);
+          }
+        }
+      }
+      System.out.println(rtnArray);
+      //System.out.println("Connected component from S =  " + s.name + ": " + rtnArray);
+      System.out.println("\n");
+
+      // Empty arrays for next iteration
+      tempArray.removeAll(tempArray);
+      rtnArray.removeAll(rtnArray);
+    }
   }
 }
--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@
 public class Main {
   public static void main(String args[]) {
     testOne();
-//  testTwo();
+    testTwo();
   }
 
   public static void testOne() {
